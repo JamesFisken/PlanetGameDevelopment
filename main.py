628e25f04ecf55ca03aad6f9b19df4c35da0e43d
JamesFisken <99928794+JamesFisken@users.noreply.github.com>
2023-05-08 14:38:07 +1200

    Added more Title screen functionality
    -Added Buttons
    -Settings button is attached to a settings page

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
-#imports
+# imports
 import sys
 import math
 import pygame
 from pygame.locals import *
+import random
+import time
 
 # Colours
 BACKGROUND = (5, 10, 51)
@@ -17,17 +18,36 @@ gameState = {
     "onLoad": False
 }
 
-
 # Pygame Variables and setup
 pygame.init()
 fps = 60
-fpsClock = pygame.time.Clock()
+running = True
+clock = pygame.time.Clock()
+dt = 1  # delta time, so that the speed of the game isn't dependant on fps
 
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
 
+
+# classes
+
+class Button:
+    def __init__(self, x, y, *size):
+        self.x = x
+        self.y = y
+
+        if len(size) == 1:
+            self.shape = "Circle"
+            self.radius = size
+        elif len(size) == 2:
+            self.shape = "Rectangle"
+            self.xLength = size[0]
+            self.yLength = size[1]
+
+
+x = 0
 # Game loop.
-while True:
+while running:
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -38,7 +58,7 @@ while True:
 
     # Draw.
     if gameState.get("onTitleScreen"):
-        print("on title screen")
+        screen.blit("")
     elif gameState.get("onLoad"):
         print("on load game screen")
     elif gameState.get("onSettings"):
@@ -46,6 +66,8 @@ while True:
     elif gameState.get("onGame"):
         print("playing the main game")
 
-
+    x += 100 * dt
+    pygame.draw.rect(screen, (100, 100, 100), pygame.Rect(x, 50, 50, 50))
+    dt = clock.tick(fps) / 1000
     pygame.display.flip()
-    fpsClock.tick(fps)
+
