ad83f100944b39dac24f48cf8697cffeb63a1ec4
JamesFisken <99928794+JamesFisken@users.noreply.github.com>
2023-05-22 14:41:31 +1200

    fixed gravity
    Fixed gravity mechanic which wasn’t working properly due to adding the force straight to the velocity without calculating acceleration
    
    Put things like mass and gravity in terms of real world constants
    
    Added a time multiplier which speeds up the simulation without affecting gravity(that much)

--- SolarController/main.py
+++ SolarController/main.py
@@ -25,26 +25,34 @@ gameState = {
 
 # Pygame Variables and setup
 pygame.init()
-fps = 240
+fps = 500
 running = True
 clock = pygame.time.Clock()
+
 dt = 1  # delta time, so that the speed of the game isn't dependant on fps
+timeMultiplier = 3
+
 
 width, height = 1600, 900
 scaleX, scaleY = width / 1920, height / 1080
 scaleSize = min(width / 1920, height / 1080)
 
-G_CONSTANT = 6.6743 * (10 ** 11)
+# constants
+G_CONSTANT = 6.6743 * (10 ** -11)
+SOLAR_MASS = 1.989 * 10 ** 30
 
 screen = pygame.display.set_mode((width, height))
 
 # Load sprites
+# backgrounds
 titleScreen = pygame.image.load('Sprites/BaseScreens/TitleScreen.png').convert_alpha()
 settingsScreen = pygame.image.load('Sprites/BaseScreens/Settings.png').convert_alpha()
 newGamePage = pygame.image.load('Sprites/BaseScreens/NewGamePage.png').convert_alpha()
 loadGamePage = pygame.image.load('Sprites/BaseScreens/LoadGamePage.png').convert_alpha()
 
+# sprites
 Earth = pygame.image.load('Sprites/Earth2.png').convert_alpha()
+GasGiant = pygame.image.load('Sprites/GasGiant2.png').convert_alpha()
 
 # Transform sprites to the correct size
 titleScreen = pygame.transform.scale(titleScreen, (width, height))
@@ -58,57 +66,72 @@ class CelestialBody:
     def __init__(self, x, y, velX, velY, mass, radius, sprite):
         self.x = x
         self.y = y
+
         self.velX = velX
         self.velY = velY
+
+        self.forceX = 50
+        self.forceY = 0
 
         self.mass = mass
         self.radius = radius
         self.sprite = pygame.transform.scale(sprite, (
-        radius, radius))  # converts the size of the sprite to fit that of the planets true radius
+            radius, radius))  # converts the size of the sprite to fit that of the planets true radius
+
+
 
     def update(self):
-        # change the position by the velocity with respect to time(dt)
+        accelX = self.forceX / self.mass
+        accelY = self.forceY / self.mass
+
+        self.velX += accelX * dt
+        self.velY += accelY * dt
+
         self.x += self.velX * dt
         self.y += self.velY * dt
 
+        self.forceX = 0
+        self.forceY = 0
+        pixels.append([self.x, self.y])
+
+
     def display(self):
-
-
+        for x in pixels:
+            pygame.draw.circle(screen,(255, 0, 0), (x[0], x[1]), 2)
         screen.blit(self.sprite, (self.x - self.sprite.get_width() / 2, self.y - self.sprite.get_height() / 2))
-        pygame.draw.circle(screen, (0, 0, 255), (self.x, self.y),
-                           self.radius * 0.3)
+        # pygame.draw.circle(screen, (0, 0, 255), (self.x, self.y),
+        # self.radius * 0.3)
+
+    def apply_gravity(self, otherBodies):
+        netForceX = 0
+        netForceY = 0
+        for body in otherBodies:  # loop through all the other planets/suns excluding the planet your looking at
+            distance = math.sqrt(((self.x - body.x) ** 2 + (
+                    self.y - body.y) ** 2))  # calculate the distance between the 2 planets
+            m1 = self.mass
+            m2 = body.mass
+
+            # dir = math.atan2(body.x - self.x, body.y - self.y)
+            Force = (m1 * m2) / (distance ** 2) * G_CONSTANT  # calculate the total force
 
 
-p1 = CelestialBody(650, 450, 0, 60, 10, 10, Earth)
-p2 = CelestialBody(700, 450, 0, 0, 10, 10, Earth)
+            normalizedDir = preprocessing.normalize(np.array([(body.x - self.x), (body.y - self.y)]).reshape(1,
+                                                                                                             -1))  # normalize the distances, so that they can act as a direction
+
+            # split the direction into x and y components and then multiply by the force
+            netForceX += normalizedDir[0][0] * Force
+            netForceY += normalizedDir[0][1] * Force
+
+        self.forceX += netForceX
+        self.forceY += netForceY
+
+        return [netForceX, netForceY]
 
 
-def apply_gravity(celestialBody, otherBodies):
-    forceDirectionPairs = []
-    for body in otherBodies:  # loop through all the other planets/suns excluding the planet your looking at
-        distance = math.sqrt(((celestialBody.x - body.x) ** 2 + (
-                    celestialBody.y - body.y) ** 2))  # calculate the distance between the 2 planets
-        m1 = celestialBody.mass
-        m2 = body.mass
-
-        #dir = math.atan2(body.x - celestialBody.x, body.y - celestialBody.y)
-        Force = (m1 * m2) / (distance**2) * 30 # calculate the total force
-
-        normalizedDir = preprocessing.normalize(np.array([(body.x - celestialBody.x), (body.y - celestialBody.y)]).reshape(1, -1))  # normalize the distances, so that they can act as a direction
-
-        # split the direction into x and y components and then multiply by the force
-        forceX = normalizedDir[0][0] * Force
-        forceY = normalizedDir[0][1] * Force
-
-
-        celestialBody.velX += forceX
-        celestialBody.velY += forceY
-
-    return Force
-
-
-apply_gravity(p1, [p2])
-
+p1 = CelestialBody(400, 450, 0, 40, 100, 200, GasGiant)
+p2 = CelestialBody(800, 450, 0, 0, SOLAR_MASS*0.00000000000001, 200, Earth)  # sun
+p3 = CelestialBody(1000, 450, 0, 70, 80, 100, Earth)  # sun
+pixels = []
 
 def change_scene(newScene, gameState, buttons):
     for key in gameState.keys():
@@ -126,10 +149,6 @@ def change_scene(newScene, gameState, buttons):
 buttons = gamebuttons.gather_buttons(scaleX, scaleY, scaleSize, screen)  # setups the buttons in the gamebuttons file
 change_scene("onTitleScreen", gameState, buttons)  # start the game on the title screen
 
-
-
-
-
 # Game loop.
 while running:
 
@@ -143,11 +162,16 @@ while running:
 
         p2.update()
         p2.display()
+        p2.apply_gravity([p1])
         p1.update()
         p1.display()
-        apply_gravity(p1, [p2])
+        p1.apply_gravity([p2])
 
-        #apply_gravity(p2, [p1])
+        p3.update()
+        p3.display()
+        p3.apply_gravity([p2])
+
+        # apply_gravity(p2, [p1])
     elif gameState.get("onLoadGame"):
         screen.blit(loadGamePage, (0, 0))
     elif gameState.get("onNewGame"):
@@ -235,7 +259,8 @@ while running:
         pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
     # Update.
-    dt = clock.tick(fps) / 1000  # find the amount of time between each frame in seconds,
-    print("FPS:", int(clock.get_fps()))
-    pygame.display.update()
+    dt = clock.tick(fps) / 1000 * timeMultiplier  # find the amount of time between each frame in seconds,
 
+    print("FPS:", int(clock.get_fps()))
+
+    pygame.display.update()
